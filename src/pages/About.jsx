7320fbd1234a2f49cd1c25132d283214af06f69c
Levi-Pronk-Jones <levi.pronkjones@student.hagley.school.nz>
2025-08-08 11:12:39 +1200

    Simplified website back down to base html website

--- src/pages/About.jsx
+++
@@ -1,8 +0,0 @@
-const About  = () => {
-    return <div>
-        <h1>About</h1>
-        <h3>This is where You learn about what My variation of the Godot Docs are.</h3>
-    </div>
-}
-
-export default About;
